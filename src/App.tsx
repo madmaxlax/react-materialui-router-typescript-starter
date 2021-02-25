a7e251b73f2f1703dc481d9d2fb0e0ab0667a3de
Max Struever <struever.max@bcgplatinion.com>
2021-02-25 17:07:32 -0500

    feat(eslint): all working
    thanks to https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Box, Container, Link, Typography } from '@material-ui/core';
+import React from 'react';
+import ProTip from './ProTip';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example with TypeScript
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
