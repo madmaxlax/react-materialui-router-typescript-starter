d34ae90e2fd121987c1e564c2109e0e3b4f8f52a
Max Struever <madmaxlax@users.noreply.github.com>
2021-03-03 11:09:04 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import clsx from 'clsx';
 import React from 'react';
@@ -9,6 +9,7 @@ import { Link } from './Link';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const useStyles = makeStyles((theme: CustomTheme) => ({
   footerRoot: {},
+  footerContainer: { minHeight: 100 },
 }));
 
 const Footer = (props: ClassNameProps) => {
@@ -16,16 +17,24 @@ const Footer = (props: ClassNameProps) => {
   const classes = useStyles();
 
   return (
-    <div className={clsx(classes.footerRoot, className)}>
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" to="https://material-ui.com/">
-          Your Website
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    </div>
+    <Grid
+      container
+      direction="column"
+      justifyContent="space-between"
+      alignItems="stretch"
+      className={clsx(classes.footerRoot, className)}
+    >
+      <Grid item container justifyContent="center" alignItems="center" className={classes.footerContainer}>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {'Copyright © '}
+          <Link color="inherit" to="https://material-ui.com/">
+            Your Website
+          </Link>{' '}
+          {new Date().getFullYear()}
+          {'.'}
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
 
